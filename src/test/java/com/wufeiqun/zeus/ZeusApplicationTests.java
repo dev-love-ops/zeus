76a0fdeaa94af10aec94a48a354cac9d7d0ef872
wufeiqun <wufeiqun@pharmcube.com>
2025-03-17 15:59:29 +0800

    first commit

---
+++ src/test/java/com/wufeiqun/zeus/ZeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wufeiqun.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
